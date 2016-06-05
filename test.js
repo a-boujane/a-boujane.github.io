999f1c96150689ff692a7e9db34195866b8c8095
abe-Desktop <abe.boujane@gmail.com>
2016-06-05 12:31:28 -0400

    adding graph node when clicking the page

--- test.js
+++ test.js
@@ -1,7 +1,21 @@
+var i = 0;
 
-function printMousePos(event) {
-  document.body.textContent =
-    "clientX: " + event.clientX +
-    " - clientY: " + event.clientY;
+function createGraphNode(event) {
+  i++;
+  var node = document.createElement("DIV");
+
+  var att1 = document.createAttribute("id");
+  att1.value = i;
+  node.setAttributeNode(att1);
+
+  var att2 = document.createAttribute("class");
+  att2.value="full-circle";
+  node.setAttributeNode(att2);
+
+  node.style.position = "absolute";
+  node.style.top = event.clientY-15+"px";
+  node.style.left = event.clientX-15+"px";
+
+  document.getElementById("graph").appendChild(node);
 }
-document.addEventListener("click", printMousePos);
+document.addEventListener("click", createGraphNode);
