26f79e1895035d64b91c25eaf42a74969951075b
abe-Desktop <abe.boujane@gmail.com>
2016-06-05 20:17:38 -0400

    Numbered nodes + focus on hover

--- test.js
+++ test.js
@@ -2,20 +2,35 @@ var i = 0;
 
 function createGraphNode(event) {
   i++;
+
   var node = document.createElement("DIV");
 
-  var att1 = document.createAttribute("id");
-  att1.value = i;
-  node.setAttributeNode(att1);
+  var att = document.createAttribute("id");
+  att.value = i;
+  node.setAttributeNode(att);
 
-  var att2 = document.createAttribute("class");
-  att2.value="full-circle";
-  node.setAttributeNode(att2);
+  att = document.createAttribute("class");
+  att.value="full-circle";
+  node.setAttributeNode(att);
+
+  var childNode = document.createElement("div");
+
+  att = document.createAttribute("class");
+  att.value="nodeText";
+
+  childNode.setAttributeNode(att);
+  childNode.textContent = i;
+
+  node.appendChild(childNode);
 
   node.style.position = "absolute";
   node.style.top = event.clientY-15+"px";
   node.style.left = event.clientX-15+"px";
 
+//  node.textContent = i;
+
   document.getElementById("graph").appendChild(node);
 }
+
+
 document.addEventListener("click", createGraphNode);
